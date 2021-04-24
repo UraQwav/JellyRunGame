f87b14370b2c3c9fd1de950a70f6e8823eb41903
uraqwav <ura.qwav@gmail.com>
2021-04-24 12:34:33 +0300

    re added scripts

---
+++ Assets/Scripts/GameOverTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GameManager.Instance.ShowEndCard();
+    }
+
+}
